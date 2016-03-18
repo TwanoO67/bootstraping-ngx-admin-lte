1febbad1206ff047cd1f7384c932e28a684cac85
WEBER Antoine <ghostsmaker@hotmail.com>
2016-03-18 16:41:37 +0100

    passage en TS compilé, et installation de lite-server

--- app/app.ts
+++ app/app.ts
@@ -1,14 +1,19 @@
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {HelloWorld} from './hello_world';
 import {Header} from './header';
 import {MenuAside} from './menu-aside';
 
+@RouteConfig([
+  {path:'/', name: 'Home', component: HelloWorld},
+  {path:'/hello',        name: 'HelloWorld',       component: HelloWorld}
+])
 @Component({
   // Declare the tag name in index.html to where the component attaches
   selector: 'app',
   // Location of the template for this component
   templateUrl: 'app/app.html',
-  directives: [Header, MenuAside, HelloWorld]
+  directives: [Header, MenuAside, ROUTER_DIRECTIVES],
 })
 export class App {
   // Declaring the variable for binding with initial value
