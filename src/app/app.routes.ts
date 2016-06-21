dbb13cfb183781b9938debdcc5371c8ec3d09f12
WEBER Antoine <ghostsmaker@hotmail.com>
2016-06-21 23:46:33 +0200

    updated to router alpha7

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,14 @@ import {Page2Component} from './+page2';
 import {Page3Component} from './+page3';
 
 export const routes: RouterConfig = [
-  {path: '/home', component: HomeComponent, index: true},
-  {path: '/page2', component: Page2Component},
-  {path: '/page3', component: Page3Component}
+  {
+    path: '',
+    redirectTo: 'home',
+    terminal: true
+  },
+  {path: 'home', component: HomeComponent},
+  {path: 'page2', component: Page2Component},
+  {path: 'page3', component: Page3Component}
 ];
 
 export const APP_ROUTER_PROVIDERS = [
