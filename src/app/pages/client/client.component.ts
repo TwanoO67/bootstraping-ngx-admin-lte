98243cb31afb825d5696fd834af386a5fcf6e0bd
Dominic Lapointe <doum@Dominics-MacBook-Pro.local>
2016-11-04 14:44:10 -0400

    minor fix again

--- src/app/pages/client/client.component.ts
+++ src/app/pages/client/client.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FirebaseListObservable } from 'angularfire2';
 import { Client } from '../../models/client';
 import { ClientDAL } from '../../dal/client.dal';
