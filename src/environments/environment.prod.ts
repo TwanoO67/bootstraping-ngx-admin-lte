bb3fb8070b4e6d96ea71550db74c8563e0e380da
WEBER Antoine <ghostsmaker@hotmail.com>
2016-11-13 10:35:37 +0100

    env for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,15 @@
 export const environment = {
-  production: true,
+  production: false,
   //THIS ARE TESTING CONFIG! DONT USE THAT IN PROD! CHANGE THAT!
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    storageBucket: "",
-    messagingSenderId: ""
+    apiKey: "AIzaSyC4mOkOIiMfgzrKE5oIMvI51FJaMZ7DwKA",
+    authDomain: "ng2-admin-lte-a3958.firebaseapp.com",
+    databaseURL: "https://ng2-admin-lte-a3958.firebaseio.com",
+    storageBucket: "ng2-admin-lte-a3958.appspot.com",
+    messagingSenderId: "201342590340"
   },
   auth0: {
-    clientID: '',
-    domain: ''
+    clientID: 'HEkxSwd54g8LXfr3P8g72tvmckSIF0KC',
+    domain: 'ng2-admin-lte.eu.auth0.com'
   }
 };
