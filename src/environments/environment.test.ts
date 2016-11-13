6487733fd26ae25054f65dc63c451a2211af1a42
WEBER Antoine <ghostsmaker@hotmail.com>
2016-11-13 10:31:33 +0100

    add testing env

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  //THIS ARE TESTING CONFIG! DONT USE THAT IN PROD! CHANGE THAT!
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    storageBucket: "",
+    messagingSenderId: ""
+  },
+  auth0: {
+    clientID: '',
+    domain: ''
+  }
+};
