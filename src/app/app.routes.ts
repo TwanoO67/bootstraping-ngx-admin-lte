6dba2fb40da60adad4c544374de3ab9aba846425
WEBER Antoine <ghostsmaker@hotmail.com>
2017-08-03 12:24:56 +0200

    adding a route for /

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -19,6 +19,11 @@ const routes: Routes = [
       {
         canActivate: [CanActivateGuard],
         component: HomeComponent,
+        path: ''
+      },
+      {
+        canActivate: [CanActivateGuard],
+        component: HomeComponent,
         path: 'home'
       },
       {
