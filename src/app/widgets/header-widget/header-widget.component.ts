c918ac442457285465b7127cc30388496242e64d
WEBER Antoine <ghostsmaker@hotmail.com>
2017-11-05 14:38:48 +0100

    component integration

--- src/app/widgets/header-widget/header-widget.component.ts
+++ src/app/widgets/header-widget/header-widget.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
+import { Component, OnInit, OnChanges, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-header-widget',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './header-widget.component.html'
 })
 export class HeaderWidgetComponent implements OnInit, OnChanges {
--- src/app/widgets/menu-widget/menu-widget.component.ts
+++ src/app/widgets/menu-widget/menu-widget.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
+import { Component, OnInit, OnChanges, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-menu-widget',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './menu-widget.component.html'
 })
 export class MenuWidgetComponent implements OnInit, OnChanges {
